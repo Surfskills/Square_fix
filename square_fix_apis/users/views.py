1fad199f9abaeb7fdf861dc0738d3483420b355a
Surfskills <ojjfred@gmail.com>
2024-03-12 23:10:08 +0000

    User login api

--- square_fix_apis/users/views.py
+++ square_fix_apis/users/views.py
@@ -7,8 +7,6 @@ from .serializers import IMUserSerializer
 from .models import IMUser
 from django.contrib.auth import authenticate
 
-
-
 #Registration
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -27,16 +25,7 @@ def signup(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    #User signing in
-from .serializers import IMUserSerializer
-
-from django.contrib.auth import authenticate, login
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
-from rest_framework import status
-from .serializers import IMUserSerializer
-
+#User signing in
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def user_login(request):
